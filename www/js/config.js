c8b8496cc64a014e2d93a9186da54b317647c0b4
Niranjan Rajendran <niranjan94@yahoo.com>
2016-08-23 19:38:10 +0530

    Added app configuration

--- www/js/config.js
+++ www/js/config.js
@@ -1,3 +1,14 @@
-/**
- * Created by Niranjan on 23-Aug-16.
- */
+var config = {
+    'APP_CONFIG': {
+        'API_ENDPOINT': 'https://open-event-dev.herokuapp.com/api/v2',
+        'WHITELISTED_DOMAINS': ['open-event-dev.herokuapp.com']
+    }
+};
+
+config_module = angular.module('app.config', []);
+
+angular.forEach(config, function(key,value) {
+    config_module.constant(value,key);
+});
+
+module.exports = config_module;
