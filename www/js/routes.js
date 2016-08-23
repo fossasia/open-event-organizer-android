adf5c19c9ca259b91df775dfbbd88fa12ff585e3
Niranjan Rajendran <niranjan94@yahoo.com>
2016-08-23 19:38:46 +0530

    Added basic login

--- www/js/routes.js
+++ www/js/routes.js
@@ -4,18 +4,19 @@
 // Set up the various states which the app can be in.
 // Each state's controller can be found in controllers.js
 
-angular.module('app.routes', [])
+module.exports = angular.module('app.routes', [])
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('login', {
-                url: '/',
+                url: '/login',
                 templateUrl: 'templates/login.html',
+                skipAuthorization: true,
                 controller: 'loginCtrl'
             })
             .state('dashboard', {
-                url: '/dashboard',
+                url: '/',
                 templateUrl: 'templates/dashboard.html',
                 controller: 'dashboardCtrl'
             });
-        $urlRouterProvider.otherwise('/')
+        $urlRouterProvider.otherwise('/login')
     });
