ac68203ca2268472ca280a453652d4ff90412359
Niranjan Rajendran <niranjan94@yahoo.com>
2016-08-23 19:38:36 +0530

    Added jwt authentication handling

--- www/js/app.js
+++ www/js/app.js
@@ -4,8 +4,31 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives', 'app.services',])
-    .run(function ($ionicPlatform) {
+require('./config');
+require('./controllers');
+require('./directives');
+require('./routes');
+require('angular-jwt');
+require('./services');
+
+angular.module('app', ['ionic', 'angular-jwt', 'app.config', 'app.controllers', 'app.routes', 'app.directives', 'app.services'])
+    .config(function Config($httpProvider, jwtOptionsProvider, APP_CONFIG) {
+        jwtOptionsProvider.config({
+            unauthenticatedRedirectPath: '/login',
+            whiteListedDomains: APP_CONFIG.WHITELISTED_DOMAINS,
+            tokenGetter: ['options', function (options) {
+                // Skip authentication for template requests
+                if (options.url.substr(options.url.length - 5) == '.html') {
+                    return null;
+                }
+                return localStorage.getItem('access_token');
+            }]
+        });
+        $httpProvider.interceptors.push('jwtInterceptor');
+    })
+    .run(function ($ionicPlatform, authManager) {
+        authManager.redirectWhenUnauthenticated();
+        authManager.checkAuthOnRefresh();
         $ionicPlatform.ready(function () {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
@@ -15,7 +38,8 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
             }
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
+                //noinspection NodeModulesDependencies
                 StatusBar.styleDefault();
             }
         });
-    })
+    });
