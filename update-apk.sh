20d18033b1d3fdcf7b7b633633ada12328a472e6
the-dagger <harshithdwivedi@gmail.com>
2016-08-27 01:58:21 +0530

    Changed links to point to fossasia repo

--- update-apk.sh
+++ update-apk.sh
@@ -1,11 +1,11 @@
 mkdir $HOME/daily/
-cp -R /home/travis/build/the-dagger/open-event-orga-app/platforms/android/build/outputs/apk/android-debug.apk $HOME/daily/
+cp -R /home/travis/build/fossasia/open-event-orga-app/platforms/android/build/outputs/apk/android-debug.apk $HOME/daily/
 # go to home and setup git
 cd $HOME
   git config --global user.email "harshithdwivedi@gmail.com"
   git config --global user.name "the-dagger"
 
-git clone --quiet --branch=gh-pages https://the-dagger:$GITHUB_API_KEY@github.com/the-dagger/open-event-orga-app  gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://the-dagger:$GITHUB_API_KEY@github.com/fossasia/open-event-orga-app  gh-pages > /dev/null
 cd gh-pages
 cp -Rf $HOME/daily/*  sample-apk/
 git add -f .
