65172bdbb5b3e8836c88b6661ae73bbd66185410
the-dagger <harshithdwivedi@gmail.com>
2016-08-27 00:28:10 +0530

    Add sample apk after each comit

---
+++ update-apk.sh
@@ -0,0 +1,16 @@
+mkdir $HOME/daily/
+cp -R /home/travis/build/the-dagger/open-event-orga-app/platforms/android/build/outputs/apk/android-debug.apk $HOME/daily/
+# go to home and setup git
+cd $HOME
+  git config --global user.email "harshithdwivedi@gmail.com"
+  git config --global user.name "the-dagger"
+
+git clone --quiet --branch=gh-pages https://the-dagger:$GITHUB_API_KEY@github.com/the-dagger/open-event-orga-app  gh-pages > /dev/null
+cd gh-pages
+cp -Rf $HOME/daily/*  sample-apk/
+git add -f .
+  # git remote rm origin
+  # git remote add origin https://the-dagger:$GITHUB_API_KEY@github.com/the-dagger/open-event-android
+  git add -f .
+  git commit -m "Update Sample Apk [skip ci]"
+  git push origin gh-pages > /dev/null
