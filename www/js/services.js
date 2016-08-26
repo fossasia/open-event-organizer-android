6cb43d73ce4a0d0d636876aa8de9832c5458abd4
Niranjan Rajendran <niranjan94@yahoo.com>
2016-08-26 08:43:14 +0530

    Added attendance load service and a builder for promise

--- www/js/services.js
+++ www/js/services.js
@@ -1,12 +1,26 @@
+function getPromise(deferred) {
+    var promise = deferred.promise;
+    promise.success = function (fn) {
+        promise.then(fn);
+        return promise;
+    };
+    promise.error = function (fn) {
+        promise.then(null, fn);
+        return promise;
+    };
+    return promise
+}
+
 module.exports = angular.module('app.services', [])
     .factory('verifyToken', ['$location', '$q', 'jwtHelper', function ($location, $q, jwtHelper) {
-        var defer = $q.defer();
+        var deferred = $q.defer();
+        var promise = getPromise(deferred);
         var accessToken = localStorage.getItem('access_token');
         if (!accessToken || jwtHelper.isTokenExpired(accessToken)) {
-            defer.reject();
+            deferred.reject();
             $location.path("/login").replace();
         } else {
-            defer.resolve('valid');
+            deferred.resolve('valid');
         }
         return defer.promise;
     }])
@@ -14,8 +28,7 @@ module.exports = angular.module('app.services', [])
         return {
             loginUser: function (email, password) {
                 var deferred = $q.defer();
-                var promise = deferred.promise;
-
+                var promise = getPromise(deferred);
                 $http.post(APP_CONFIG.API_ENDPOINT + "/login", {
                     email: email,
                     password: password
@@ -34,14 +47,6 @@ module.exports = angular.module('app.services', [])
                         deferred.reject('Could not connect to the server.');
                     }
                 });
-                promise.success = function (fn) {
-                    promise.then(fn);
-                    return promise;
-                };
-                promise.error = function (fn) {
-                    promise.then(null, fn);
-                    return promise;
-                };
                 return promise;
             }
         }
@@ -49,7 +54,7 @@ module.exports = angular.module('app.services', [])
         return {
             loadEvents: function () {
                 var deferred = $q.defer();
-                var promise = deferred.promise;
+                var promise = getPromise(deferred);
                 $http.get(APP_CONFIG.API_ENDPOINT + "/users/me/events")
                     .then(function successCallback(response) {
                         deferred.resolve(response.data);
@@ -61,14 +66,44 @@ module.exports = angular.module('app.services', [])
                             deferred.reject('Could not connect to the server.');
                         }
                     });
-                promise.success = function (fn) {
-                    promise.then(fn);
-                    return promise;
-                };
-                promise.error = function (fn) {
-                    promise.then(null, fn);
-                    return promise;
-                };
+                return promise;
+            }
+        }
+    }).service('LoadEventService', function ($q, $http, APP_CONFIG) {
+        return {
+            loadEvent: function (eventId) {
+                var deferred = $q.defer();
+                var promise = getPromise(deferred);
+                $http.get(APP_CONFIG.API_ENDPOINT + "/events/" + eventId + "?include=tickets")
+                    .then(function successCallback(response) {
+                        deferred.resolve(response.data);
+                    }, function errorCallback(response) {
+                        console.log(response);
+                        if (response.status == 401) {
+                            deferred.reject('auth_failed');
+                        } else {
+                            deferred.reject('Could not connect to the server.');
+                        }
+                    });
+                return promise;
+            }
+        }
+    }).service('LoadAttendeesService', function ($q, $http, APP_CONFIG) {
+        return {
+            loadAttendees: function (eventId) {
+                var deferred = $q.defer();
+                var promise = getPromise(deferred);
+                $http.get(APP_CONFIG.API_ENDPOINT + "/events/" + eventId + "/attendees/")
+                    .then(function successCallback(response) {
+                        deferred.resolve(response.data);
+                    }, function errorCallback(response) {
+                        console.log(response);
+                        if (response.status == 401) {
+                            deferred.reject('auth_failed');
+                        } else {
+                            deferred.reject('Could not connect to the server.');
+                        }
+                    });
                 return promise;
             }
         }
