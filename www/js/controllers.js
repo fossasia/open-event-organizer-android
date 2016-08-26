63fc0d5d5b49396e01669cc3b87bb70f5f10505f
Niranjan Rajendran <niranjan94@yahoo.com>
2016-08-26 08:44:57 +0530

    Added tickets and attendance progress bars to the dashboard

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -31,40 +31,84 @@ module.exports = angular.module('app.controllers', [])
         }
     })
     .controller('eventPickerCtrl', function ($scope, $stateParams, $ionicNavBarDelegate, $location, LoadEventsService, $localStorage, verifyToken) {
-        $ionicNavBarDelegate.showBackButton(false);
         $scope.events = [];
         $scope.isLoading = true;
         $scope.$storage = $localStorage;
-        verifyToken.then(function () {
-            $scope.pickEvent = function (event) {
-                $scope.$storage.selectedEvent = event;
-                $location.path("/event").replace();
-            };
+        $scope.pickEvent = function (event) {
+            $scope.$storage.selectedEvent = event;
+            $location.path("/event").replace();
+        };
 
-            LoadEventsService
-                .loadEvents()
-                .success(function (data) {
-                    $scope.events = data;
+        LoadEventsService
+            .loadEvents()
+            .success(function (data) {
+                $scope.events = data;
+                $scope.isLoading = false;
+            })
+            .error(function (data) {
+                if(data === 'auth_failed') {
+                    $location.path("/login").replace();
+                } else {
+                    $ionicPopup.alert({
+                        title: 'Data loading failed',
+                        template: data
+                    });
                     $scope.isLoading = false;
+                }
+            });
+    })
+    .controller('eventDashboardCtrl', function ($scope, $stateParams, $ionicNavBarDelegate, $location, LoadEventsService, $localStorage, verifyToken, LoadEventService, LoadAttendeesService) {
+        $scope.isLoading = false;
+        $scope.$storage = $localStorage;
+        $scope.stats = {
+            tickets: {
+                sold: 0,
+                total: 0
+            },
+            attendees: {
+                total: 0,
+                present: 0
+            }
+        };
+
+        $scope.percentage = function(value, total){
+            if (total <= 0) {
+                return 0;
+            }
+            var result = ((value/total)*100);
+            return Math.round(result);
+        };
+
+        verifyToken.then(function () {
+            LoadEventService
+                .loadEvent($localStorage.selectedEvent.id)
+                .success(function (data) {
+                    $localStorage.selectedEvent = data;
+                    angular.forEach(data.tickets, function(ticket) {
+                        $scope.stats.tickets.total += ticket.quantity;
+                    });
                 })
                 .error(function (data) {
                     if(data === 'auth_failed') {
                         $location.path("/login").replace();
                     } else {
-                        $ionicPopup.alert({
-                            title: 'Data loading failed',
-                            template: data
-                        });
-                        $scope.isLoading = false;
+
                     }
                 });
-        });
-    })
-    .controller('eventDashboardCtrl', function ($scope, $stateParams, $ionicNavBarDelegate, $location, LoadEventsService, $localStorage, verifyToken) {
-        $scope.isLoading = false;
-        $scope.$storage = $localStorage;
-        $ionicNavBarDelegate.showBackButton(false);
-        verifyToken.then(function () {
+            LoadAttendeesService
+                .loadAttendees($localStorage.selectedEvent.id)
+                .success(function (data) {
+                    $scope.stats.attendees.total = $scope.stats.tickets.sold = data.length;
+                    angular.forEach(data, function(attendee) {
+                        $scope.stats.tickets.total += (attendee.checked_in ? 1 : 0);
+                    });
+                })
+                .error(function (data) {
+                    if(data === 'auth_failed') {
+                        $location.path("/login").replace();
+                    } else {
 
+                    }
+                });
         });
     });
