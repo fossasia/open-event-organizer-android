01b3b1162fa8311fa54fb7fcc7bc17a6853279d8
Niranjan Rajendran <niranjan94@yahoo.com>
2016-09-04 18:55:41 +0530

    Added interfaces for all API responses

---
+++ app/interfaces/order.ts
@@ -0,0 +1,10 @@
+export interface Order {
+  status: string;
+  completed_at: Date;
+  paid_via: string;
+  id: number;
+  payment_mode: string;
+  invoice_number: string;
+  amount: number;
+  identifier: string;
+}
