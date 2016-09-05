1f4a945812d30933f9c81fd78593a7c409220756
Niranjan Rajendran <niranjan94@yahoo.com>
2016-09-05 15:15:12 +0530

    Fixed updated script

--- scripts/update-apk.sh
+++ scripts/update-apk.sh
@@ -9,9 +9,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-
     exit 0
 fi
 
-git clone --quiet --branch=apk https://the-dagger:$GITHUB_API_KEY@github.com/fossasia/open-event-orga-app  apk > /dev/null
+git clone --quiet --branch=apk https://the-dagger:$GITHUB_API_KEY@github.com/fossasia/open-event-orga-app apk > /dev/null
 cp platforms/android/build/outputs/apk/android-debug.apk apk/test-android-debug.apk
 cd apk
 git add test-android-debug.apk
 git commit -m "[Auto] Update Test Apk"
-git push origin apk > /dev/null
+git push origin apk --quiet > /dev/null
