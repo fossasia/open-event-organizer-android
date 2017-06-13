9c33f3179e8338de988e81963887479e9f2a8a10
mejariamol <amolp.mejari.eee14@itbhu.ac.in>
2017-06-13 11:24:06 +0530

    Replaces qr scan button with fab

--- app/src/main/java/org/fossasia/openevent/app/event/EventContainerFragment.java
+++ app/src/main/java/org/fossasia/openevent/app/event/EventContainerFragment.java
@@ -80,7 +80,7 @@ public class EventContainerFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.activity_event_container, container, false);
+        View view = inflater.inflate(R.layout.fragment_event_container, container, false);
         ButterKnife.bind(this, view);
         return view;
     }
