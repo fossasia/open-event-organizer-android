6b71a79e0c28ed6463f4a2c0b60d9759c2b27d5f
Areeb Jamal <jamal.areeb@gmail.com>
2017-06-27 18:33:24 +0530

    chore: Simplify dagger implementation

--- app/src/main/java/org/fossasia/openevent/app/common/di/module/PresenterModule.java
+++ app/src/main/java/org/fossasia/openevent/app/common/di/module/PresenterModule.java
@@ -1,15 +1,9 @@
 package org.fossasia.openevent.app.common.di.module;
 
-import org.fossasia.openevent.app.data.contract.IEventRepository;
-import org.fossasia.openevent.app.data.contract.ILoginModel;
-import org.fossasia.openevent.app.data.contract.IUtilModel;
-import org.fossasia.openevent.app.data.db.contract.IDatabaseChangeListener;
-import org.fossasia.openevent.app.data.db.contract.IDatabaseRepository;
-import org.fossasia.openevent.app.data.models.Attendee;
 import org.fossasia.openevent.app.event.attendees.AttendeesPresenter;
+import org.fossasia.openevent.app.event.attendees.contract.IAttendeesPresenter;
 import org.fossasia.openevent.app.event.checkin.AttendeeCheckInPresenter;
 import org.fossasia.openevent.app.event.checkin.contract.IAttendeeCheckInPresenter;
-import org.fossasia.openevent.app.event.attendees.contract.IAttendeesPresenter;
 import org.fossasia.openevent.app.event.detail.EventDetailPresenter;
 import org.fossasia.openevent.app.event.detail.contract.IEventDetailPresenter;
 import org.fossasia.openevent.app.events.EventsPresenter;
@@ -19,40 +13,28 @@ import org.fossasia.openevent.app.login.contract.ILoginPresenter;
 import org.fossasia.openevent.app.qrscan.ScanQRPresenter;
 import org.fossasia.openevent.app.qrscan.contract.IScanQRPresenter;
 
+import dagger.Binds;
 import dagger.Module;
-import dagger.Provides;
 
-@Module(includes = { AndroidModule.class, DatabaseModule.class, ChangeListenerModule.class })
-public class PresenterModule {
+@Module
+public abstract class PresenterModule {
 
-    @Provides
-    ILoginPresenter providesLoginPresenter(ILoginModel loginModel, IUtilModel utilModel) {
-        return new LoginPresenter(loginModel, utilModel);
-    }
+    @Binds
+    abstract ILoginPresenter bindsLoginPresenter(LoginPresenter loginPresenter);
 
-    @Provides
-    IEventsPresenter providesEventPresenter(IEventRepository eventRepository, ILoginModel loginModel) {
-        return new EventsPresenter(eventRepository, loginModel);
-    }
+    @Binds
+    abstract IEventsPresenter bindsEventsPresenter(EventsPresenter eventsPresenter);
 
-    @Provides
-    IEventDetailPresenter providesEventDetailPresenter(IEventRepository eventRepository) {
-        return new EventDetailPresenter(eventRepository);
-    }
+    @Binds
+    abstract IEventDetailPresenter bindsEventDetailPresenter(EventDetailPresenter eventDetailPresenter);
 
-    @Provides
-    IAttendeesPresenter providesAttendeePresenter(IEventRepository eventRepository, IDatabaseChangeListener<Attendee> attendeeChangeListener) {
-        return new AttendeesPresenter(eventRepository, attendeeChangeListener);
-    }
+    @Binds
+    abstract IAttendeesPresenter bindsAttendeePresenter(AttendeesPresenter attendeesPresenter);
 
-    @Provides
-    IScanQRPresenter providesScanQRPresenter(IEventRepository eventRepository) {
-        return new ScanQRPresenter(eventRepository);
-    }
+    @Binds
+    abstract IScanQRPresenter bindsScanQRPresenter(ScanQRPresenter scanQRPresenter);
 
-    @Provides
-    IAttendeeCheckInPresenter providesAttendeeCheckInPresenter(IDatabaseRepository databaseRepository, IEventRepository eventRepository) {
-        return new AttendeeCheckInPresenter(databaseRepository, eventRepository);
-    }
+    @Binds
+    abstract IAttendeeCheckInPresenter bindsAttendeeCheckInPresenter(AttendeeCheckInPresenter attendeeCheckInPresenter);
 
 }
