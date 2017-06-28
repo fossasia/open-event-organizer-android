0501a8e7bfd2457570314917d4bd27422cd7c8fb
Amol Mejari <amolp.mejari.eee14@itbhu.ac.in>
2017-06-28 21:24:30 +0530

    fix: Scan QR fab visibility (#240)
    - Hides on scroll
    - Hides on empty page
    
    fixes #238

--- app/src/main/java/org/fossasia/openevent/app/utils/ViewUtils.java
+++ app/src/main/java/org/fossasia/openevent/app/utils/ViewUtils.java
@@ -2,7 +2,9 @@ package org.fossasia.openevent.app.utils;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 public class ViewUtils {
@@ -24,6 +26,20 @@ public class ViewUtils {
 
     public static void setTint(View view, String color) {
         setTint(view, Color.parseColor(color));
+    }
+
+    public static void setRecyclerViewScrollAwareFabBehaviour(RecyclerView recyclerView, FloatingActionButton fab) {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (dy > 0 && fab.getVisibility() == View.VISIBLE) {
+                    fab.hide();
+                } else if (dy < 0 && fab.getVisibility() != View.VISIBLE){
+                    fab.show();
+                }
+            }
+        });
     }
 
 }
