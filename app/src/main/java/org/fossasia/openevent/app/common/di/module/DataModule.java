5cbebd567f28abe601c465d5e2c6c9c45ec52eea
Areeb Jamal <jamal.areeb@gmail.com>
2017-07-11 22:42:51 +0530

    feat: Simplify data flow of event data

--- app/src/main/java/org/fossasia/openevent/app/common/di/module/DataModule.java
+++ app/src/main/java/org/fossasia/openevent/app/common/di/module/DataModule.java
@@ -1,13 +1,15 @@
 package org.fossasia.openevent.app.common.di.module;
 
-import org.fossasia.openevent.app.data.repository.AttendeeRepository;
-import org.fossasia.openevent.app.data.repository.EventRepository;
+import org.fossasia.openevent.app.data.Bus;
 import org.fossasia.openevent.app.data.LoginModel;
 import org.fossasia.openevent.app.data.UtilModel;
-import org.fossasia.openevent.app.data.repository.contract.IAttendeeRepository;
-import org.fossasia.openevent.app.data.repository.contract.IEventRepository;
+import org.fossasia.openevent.app.data.contract.IBus;
 import org.fossasia.openevent.app.data.contract.ILoginModel;
 import org.fossasia.openevent.app.data.contract.IUtilModel;
+import org.fossasia.openevent.app.data.repository.AttendeeRepository;
+import org.fossasia.openevent.app.data.repository.EventRepository;
+import org.fossasia.openevent.app.data.repository.contract.IAttendeeRepository;
+import org.fossasia.openevent.app.data.repository.contract.IEventRepository;
 
 import javax.inject.Singleton;
 
@@ -16,6 +18,10 @@ import dagger.Module;
 
 @Module
 public abstract class DataModule {
+
+    @Binds
+    @Singleton
+    abstract IBus bindsBus(Bus bus);
 
     @Binds
     @Singleton
