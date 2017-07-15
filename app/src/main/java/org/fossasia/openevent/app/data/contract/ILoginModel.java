ca36fa876bd2058bbffa2fec58ea12ec8fe62850
Amol Mejari <amolp.mejari.eee14@itbhu.ac.in>
2017-07-15 13:20:55 +0530

    fix: clear database on different user login (#260)

--- app/src/main/java/org/fossasia/openevent/app/data/contract/ILoginModel.java
+++ app/src/main/java/org/fossasia/openevent/app/data/contract/ILoginModel.java
@@ -1,13 +1,10 @@
 package org.fossasia.openevent.app.data.contract;
 
-import org.fossasia.openevent.app.data.models.LoginResponse;
-
 import io.reactivex.Completable;
-import io.reactivex.Observable;
 
 public interface ILoginModel {
 
-    Observable<LoginResponse> login(String username, String password);
+    Completable login(String username, String password);
 
     boolean isLoggedIn();
 
