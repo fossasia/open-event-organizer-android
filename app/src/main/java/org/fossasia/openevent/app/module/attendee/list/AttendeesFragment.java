5ade45b1a43b81d9ef04c383a74036de3d64e73e
Areeb Jamal <jamal.areeb@gmail.com>
2017-08-11 15:46:51 +0530

    chore: Prepare for release (#444)
    - Add icons
    - Update README with badges and icon
    - Change app version to alpha
    - Fix AndroidManifest backup option
    - Fix build
    
    Fixes #443

--- app/src/main/java/org/fossasia/openevent/app/module/attendee/list/AttendeesFragment.java
+++ app/src/main/java/org/fossasia/openevent/app/module/attendee/list/AttendeesFragment.java
@@ -19,7 +19,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.android.databinding.library.baseAdapters.BR;
 import com.mikepenz.fastadapter.adapters.HeaderAdapter;
