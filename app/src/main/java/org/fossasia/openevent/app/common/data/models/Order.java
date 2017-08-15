f18dd8a4d99fa91e59afa41a79c7d2c684d6fe80
Areeb Jamal <jamal.areeb@gmail.com>
2017-08-16 01:14:38 +0530

    feat: Modify order schema (#467)
    - Add/Remove relevant attributes
    - Increment DB version
    - Add version migration
    - Remove old references from layout
    
    Fixes #466

--- app/src/main/java/org/fossasia/openevent/app/common/data/models/Order.java
+++ app/src/main/java/org/fossasia/openevent/app/common/data/models/Order.java
@@ -2,6 +2,9 @@ package org.fossasia.openevent.app.common.data.models;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.github.jasminb.jsonapi.LongIdHandler;
+import com.github.jasminb.jsonapi.annotations.Id;
+import com.github.jasminb.jsonapi.annotations.Type;
 import com.raizlabs.android.dbflow.annotation.PrimaryKey;
 import com.raizlabs.android.dbflow.annotation.Table;
 
@@ -12,21 +15,39 @@ import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Type("order")
 @Builder
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @Table(database = OrgaDatabase.class, allFields = true)
+@SuppressWarnings({ "PMD.ExcessivePublicCount", "PMD.TooManyFields" })
 public class Order {
     @PrimaryKey
-    public long id;
+    @Id(LongIdHandler.class)
+    public Long id;
 
     public float amount;
     public String completedAt;
     public String identifier;
-    public String invoiceNumber;
     public String paidVia;
-    public String  paymentMode;
+    public String paymentMode;
     public String status;
+
+    // Migration 6 deletions
+    // invoiceNumber
+
+    // Migration 6 additions
+    public String address;
+    public String zipcode;
+    public String city;
+    public String state;
+    public String country;
+    public String expMonth;
+    public String expYear;
+    public String transactionId;
+    public String discountCodeId;
+    public String brand;
+    public String last4;
 
     public Order() { }
 }
