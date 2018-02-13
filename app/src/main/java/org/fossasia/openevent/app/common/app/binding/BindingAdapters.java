fc115cfe6b1c807f8824e02e6d6a96e22c7864d1
Akshay Kumar <akshaykumarsharma1998aks@gmail.com>
2018-02-13 21:25:22 +0530

    fix: App crashes when value in quantity textview is erased (#624) (#626)

--- app/src/main/java/org/fossasia/openevent/app/common/app/binding/BindingAdapters.java
+++ app/src/main/java/org/fossasia/openevent/app/common/app/binding/BindingAdapters.java
@@ -20,6 +20,7 @@ import android.widget.ImageView;
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
 import org.fossasia.openevent.app.R;
+import org.fossasia.openevent.app.common.utils.core.Utils;
 import org.fossasia.openevent.app.common.utils.ui.ViewUtils;
 
 @SuppressWarnings("PMD.AvoidCatchingGenericException")
@@ -46,8 +47,9 @@ public final class BindingAdapters {
         return value == null ? "" : String.valueOf(value);
     }
 
+    @SuppressWarnings("PMD")
     public static Long strToLong(String value) {
-        return value == null ?  null : Long.parseLong(value);
+        return Utils.isEmpty(value) ?  null : Long.parseLong(value);
     }
 
     public static Float strToFloat(String value) {
