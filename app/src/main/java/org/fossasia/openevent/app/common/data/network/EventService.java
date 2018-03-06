3757ad74bba56f98b3f80bc8660e7a4371451c41
Masquerade <masquerade0097@gmail.com>
2018-03-06 22:31:07 +0530

    fix: Minor changes

--- app/src/main/java/org/fossasia/openevent/app/common/data/network/EventService.java
+++ app/src/main/java/org/fossasia/openevent/app/common/data/network/EventService.java
@@ -94,7 +94,7 @@ public interface EventService {
     @GET("events/{eventId}/event-copyright?include=event&fields[event]=id&page[size]=0")
     Observable<Copyright> getCopyright(@Path("eventId") long eventId);
 
-    @POST("/v1/faqs")
+    @POST("faqs")
     Observable<Faq> postFaq(@Body Faq faq);
 
 }
--- app/src/main/java/org/fossasia/openevent/app/module/faq/create/CreateFaqPresenter.java
+++ app/src/main/java/org/fossasia/openevent/app/module/faq/create/CreateFaqPresenter.java
@@ -35,7 +35,6 @@ public class CreateFaqPresenter extends BasePresenter<ICreateFaqView> implements
 
     @Override
     public void createFaq() {
-
         faq.setEvent(ContextManager.getSelectedEvent());
 
         faqRepository
