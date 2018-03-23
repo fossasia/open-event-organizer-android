e0af07643f454a446e1249d7b60676e132abebb8
Masquerade <masquerade0097@gmail.com>
2018-03-23 19:21:23 +0530

    feat: Error feedback from JSON Response

--- app/src/main/java/org/fossasia/openevent/app/common/utils/core/ErrorUtils.java
+++ app/src/main/java/org/fossasia/openevent/app/common/utils/core/ErrorUtils.java
@@ -1,15 +1,11 @@
 package org.fossasia.openevent.app.common.utils.core;
 
+import org.json.JSONObject;
+
+import okhttp3.ResponseBody;
 import retrofit2.HttpException;
 
 public final class ErrorUtils {
-
-    public static final int BAD_REQUEST = 400;
-    public static final int UNAUTHORIZED = 401;
-    public static final int FORBIDDEN = 403;
-    public static final int NOT_FOUND = 404;
-    public static final int METHOD_NOT_ALLOWED = 405;
-    public static final int REQUEST_TIMEOUT = 408;
 
     private ErrorUtils() {
         // Never Called
@@ -17,23 +13,37 @@ public final class ErrorUtils {
 
     public static String getMessage(Throwable throwable) {
         if (throwable instanceof HttpException) {
-            switch (((HttpException) throwable).code()) {
-                case BAD_REQUEST:
-                    return "Something went wrong! Please check any empty field of a form.";
-                case UNAUTHORIZED:
-                    return "Invalid Credentials! Please check your credentials.";
-                case FORBIDDEN:
-                    return "Sorry, you are not authorized to make this request.";
-                case NOT_FOUND:
-                    return "Sorry, we couldn't find what you were looking for.";
-                case METHOD_NOT_ALLOWED:
-                    return "Sorry, this request is not allowed.";
-                case REQUEST_TIMEOUT:
-                    return "Sorry, request timeout. Please retry after some time.";
-                default:
-                    return throwable.getMessage();
-            }
+            ResponseBody responseBody = ((HttpException) throwable).response().errorBody();
+            return getErrorDetails(responseBody);
+        } else {
+            return throwable.getMessage();
         }
-        return throwable.getMessage();
+    }
+
+    @SuppressWarnings("PMD.AvoidCatchingGenericException")
+    private static String getErrorDetails(ResponseBody responseBody) {
+        try {
+            JSONObject jsonObject = new JSONObject(responseBody.string());
+            JSONObject jsonArray = new JSONObject(jsonObject.getJSONArray("errors").getString(0));
+            JSONObject errorSource = new JSONObject(jsonArray.getString("source"));
+
+            String pointedField = getPointedField(errorSource.getString("pointer"));
+            if (pointedField == null)
+                return jsonArray.get("detail").toString();
+            else
+                return jsonArray.get("detail").toString().replace(".", "") + ": " + pointedField;
+
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    private static String getPointedField(String pointerString) {
+        if (pointerString == null)
+            return null;
+        else {
+            String[] path = pointerString.split("/");
+            return path[path.length - 1];
+        }
     }
 }
