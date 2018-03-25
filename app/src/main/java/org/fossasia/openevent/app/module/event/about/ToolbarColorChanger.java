96083a9b58009bbf117f6d1892db3e7bf14fb1f5
Areeb Jamal <jamal.areeb@gmail.com>
2018-03-25 20:17:57 +0530

    fix: Set color of overflow dynamically (#842)

---
+++ app/src/main/java/org/fossasia/openevent/app/module/event/about/ToolbarColorChanger.java
@@ -0,0 +1,48 @@
+package org.fossasia.openevent.app.module.event.about;
+
+import android.content.res.Resources;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.view.ViewCompat;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+
+class ToolbarColorChanger {
+
+    private AppBarLayout appBarLayout;
+    private AppBarLayout.OnOffsetChangedListener onOffsetChangedListener;
+
+    @Inject
+    ToolbarColorChanger() {
+        // Default Implementation
+    }
+
+    void removeChangeListener() {
+        if (appBarLayout != null && onOffsetChangedListener != null)
+            appBarLayout.removeOnOffsetChangedListener(onOffsetChangedListener);
+    }
+
+    @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+    Observable<Integer> observeColor(AppBarLayout appBar, CollapsingToolbarLayout collapsingToolbar) {
+        appBarLayout = appBar;
+        Resources resources = appBar.getResources();
+        PublishSubject<Boolean> offsetChanger = PublishSubject.create();
+
+        onOffsetChangedListener = (appBarLayout, verticalOffset) ->
+            offsetChanger.onNext((collapsingToolbar.getHeight() + verticalOffset) <
+                (2 * ViewCompat.getMinimumHeight(collapsingToolbar)));
+        appBar.addOnOffsetChangedListener(onOffsetChangedListener);
+
+        return offsetChanger.distinctUntilChanged()
+            .map(collapsed -> {
+                if (collapsed)
+                    return resources.getColor(android.R.color.black);
+                else
+                    return resources.getColor(android.R.color.white);
+            });
+    }
+
+}
