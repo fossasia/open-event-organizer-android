3c9bf825147c665661406047d4f8c37f557ab1f8
sridharjajoo <sridharjajoo@gmail.com>
2018-03-27 19:50:11 +0530

    feat:Improved UX of faq

--- app/src/main/java/org/fossasia/openevent/app/core/faq/list/viewholder/FaqViewHolder.java
+++ app/src/main/java/org/fossasia/openevent/app/core/faq/list/viewholder/FaqViewHolder.java
@@ -1,7 +1,10 @@
 package org.fossasia.openevent.app.core.faq.list.viewholder;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
+import org.fossasia.openevent.app.R;
 import org.fossasia.openevent.app.data.models.Faq;
 import org.fossasia.openevent.app.databinding.FaqLayoutBinding;
 
@@ -12,11 +15,20 @@ public class FaqViewHolder extends RecyclerView.ViewHolder {
     public FaqViewHolder(FaqLayoutBinding binding) {
         super(binding.getRoot());
         this.binding = binding;
+        binding.answerLayout.setVisibility(View.GONE);
     }
 
     public void bind(Faq faq) {
         binding.setFaq(faq);
         binding.executePendingBindings();
+        binding.questionLayout.setOnClickListener(v -> {
+            if (binding.answerLayout.getVisibility() == View.GONE) {
+                binding.answerLayout.setVisibility(View.VISIBLE);
+                binding.addIcon.setImageDrawable(ContextCompat.getDrawable(binding.getRoot().getContext(), R.drawable.ic_arrow_up));
+            } else {
+                binding.answerLayout.setVisibility(View.GONE);
+                binding.addIcon.setImageDrawable(ContextCompat.getDrawable(binding.getRoot().getContext(), R.drawable.ic_arrow_drop_down));
+            }
+        });
     }
-
 }
