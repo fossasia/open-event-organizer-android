c508d083a2e6cfd66f1c344b704d069079d36986
srv-twry <saurav007tiwary@gmail.com>
2018-03-30 12:56:42 +0530

    fix: Remove static Copyright field from UpdateCopyrightFragment

--- app/src/main/java/org/fossasia/openevent/app/core/event/copyright/update/UpdateCopyrightFragment.java
+++ app/src/main/java/org/fossasia/openevent/app/core/event/copyright/update/UpdateCopyrightFragment.java
@@ -1,11 +1,9 @@
 package org.fossasia.openevent.app.core.event.copyright.update;
 
-import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,28 +23,34 @@ import static org.fossasia.openevent.app.ui.ViewUtils.showView;
 
 public class UpdateCopyrightFragment extends BaseBottomSheetFragment<UpdateCopyrightPresenter> implements IUpdateCopyrightView {
 
+    private static final String EVENT_ID = "id";
+
     @Inject
     Lazy<UpdateCopyrightPresenter> presenterProvider;
     private Validator validator;
     private CopyrightCreateLayoutBinding binding;
-    private static Copyright copyright;
+    private long copyrightId;
 
-    public static UpdateCopyrightFragment newInstance(Copyright copyright) {
-        UpdateCopyrightFragment.copyright = copyright;
-        return new UpdateCopyrightFragment();
+    public static UpdateCopyrightFragment newInstance(long id) {
+        Bundle bundle = new Bundle();
+        bundle.putLong(EVENT_ID, id);
+        UpdateCopyrightFragment updateCopyrightFragment = new UpdateCopyrightFragment();
+        updateCopyrightFragment.setArguments(bundle);
+        return updateCopyrightFragment;
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        final Context contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.AppTheme);
-        LayoutInflater localInflater = inflater.cloneInContext(contextThemeWrapper);
-        binding =  DataBindingUtil.inflate(localInflater, R.layout.copyright_create_layout, container, false);
+        binding =  DataBindingUtil.inflate(inflater, R.layout.copyright_create_layout, container, false);
         validator = new Validator(binding.form);
+
+        Bundle bundle = getArguments();
+        copyrightId = bundle.getLong(EVENT_ID);
 
         binding.submit.setOnClickListener(view -> {
             if (validator.validate())
-                getPresenter().updateCopyright(copyright);
+                getPresenter().updateCopyright();
         });
 
         return binding.getRoot();
@@ -56,6 +60,11 @@ public class UpdateCopyrightFragment extends BaseBottomSheetFragment<UpdateCopyr
     public void onStart() {
         super.onStart();
         getPresenter().attach(this);
+        getPresenter().loadCopyright(copyrightId);
+    }
+
+    @Override
+    public void setCopyright(Copyright copyright) {
         binding.setCopyright(copyright);
     }
 
