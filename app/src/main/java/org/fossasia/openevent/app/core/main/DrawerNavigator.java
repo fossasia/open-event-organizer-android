2889c73d0e341fba249808a080372a6ae1343aca
Areeb Jamal <jamal.areeb@gmail.com>
2018-04-03 14:52:37 +0530

    refactor: Decompose MainActivity into Navigators

---
+++ app/src/main/java/org/fossasia/openevent/app/core/main/DrawerNavigator.java
@@ -0,0 +1,55 @@
+package org.fossasia.openevent.app.core.main;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AlertDialog;
+import android.view.MenuItem;
+
+import org.fossasia.openevent.app.R;
+import org.fossasia.openevent.app.core.event.about.AboutEventActivity;
+
+class DrawerNavigator {
+
+    private final Context context;
+    private final MainPresenter mainPresenter;
+    private final FragmentNavigator fragmentNavigator;
+
+    private AlertDialog logoutDialog;
+
+    DrawerNavigator(Context context, FragmentNavigator fragmentNavigator, MainPresenter mainPresenter) {
+        this.context = context;
+        this.fragmentNavigator = fragmentNavigator;
+        this.mainPresenter = mainPresenter;
+    }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    void setLogoutDialog(AlertDialog logoutDialog) {
+        this.logoutDialog = logoutDialog;
+    }
+
+    void selectItem(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_logout)
+            showLogoutDialog();
+        else if (id == R.id.nav_about_event) {
+            Intent intent = new Intent(context, AboutEventActivity.class);
+            intent.putExtra(AboutEventActivity.EVENT_ID, fragmentNavigator.getEventId());
+            context.startActivity(intent);
+        } else
+            fragmentNavigator.loadFragment(id);
+    }
+
+    private void showLogoutDialog() {
+        if (logoutDialog == null)
+            logoutDialog = new AlertDialog.Builder(context)
+                .setTitle(R.string.logout_confirmation)
+                .setMessage(R.string.logout_confirmation_message)
+                .setPositiveButton(R.string.ok, (dialog, which) -> mainPresenter.logout())
+                .setNegativeButton(R.string.cancel, (dialog, which) -> dialog.dismiss())
+                .create();
+
+        logoutDialog.show();
+    }
+}
--- app/src/main/java/org/fossasia/openevent/app/core/main/MainActivity.java
+++ app/src/main/java/org/fossasia/openevent/app/core/main/MainActivity.java
@@ -5,27 +5,16 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AlertDialog;
 import android.view.MenuItem;
 import android.view.View;
 
 import org.fossasia.openevent.app.R;
 import org.fossasia.openevent.app.common.mvp.view.BaseInjectActivity;
-import org.fossasia.openevent.app.core.attendee.list.AttendeesFragment;
 import org.fossasia.openevent.app.core.auth.AuthActivity;
-import org.fossasia.openevent.app.core.event.about.AboutEventActivity;
-import org.fossasia.openevent.app.core.event.dashboard.EventDashboardFragment;
-import org.fossasia.openevent.app.core.event.list.EventListFragment;
-import org.fossasia.openevent.app.core.faq.list.FaqListFragment;
 import org.fossasia.openevent.app.core.organizer.detail.OrganizerDetailActivity;
-import org.fossasia.openevent.app.core.settings.SettingsFragment;
-import org.fossasia.openevent.app.core.ticket.list.TicketsFragment;
 import org.fossasia.openevent.app.data.models.Event;
 import org.fossasia.openevent.app.data.models.User;
 import org.fossasia.openevent.app.databinding.MainActivityBinding;
@@ -41,20 +30,17 @@ public class MainActivity extends BaseInjectActivity<MainPresenter> implements N
 
     public static final String EVENT_KEY = "event";
     private long eventId = -1;
-    private boolean isDashboardActive = true;
 
     @Inject
     Lazy<MainPresenter> presenterProvider;
     @Inject
     BackPressHandler backPressHandler;
 
-    private FragmentManager fragmentManager;
-    private AlertDialog logoutDialog;
+    private FragmentNavigator fragmentNavigator;
+    private DrawerNavigator drawerNavigator;
 
     private MainActivityBinding binding;
     private MainNavHeaderBinding headerBinding;
-
-    private int lastSelectedNavItemId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,7 +59,8 @@ public class MainActivity extends BaseInjectActivity<MainPresenter> implements N
         binding.navView.setNavigationItemSelectedListener(this);
 
         binding.navView.getMenu().setGroupVisible(R.id.subMenu, false);
-        fragmentManager = getSupportFragmentManager();
+        fragmentNavigator = new FragmentNavigator(getSupportFragmentManager(), eventId);
+        drawerNavigator = new DrawerNavigator(this, fragmentNavigator, getPresenter());
 
         headerBinding.profile.setOnClickListener(view -> startActivity(new Intent(this, OrganizerDetailActivity.class)));
     }
@@ -89,14 +76,12 @@ public class MainActivity extends BaseInjectActivity<MainPresenter> implements N
     public void onBackPressed() {
         if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
             binding.drawerLayout.closeDrawer(GravityCompat.START);
-        } else if (isDashboardActive) {
+        } else if (fragmentNavigator.isDashboardActive()) {
             backPressHandler.onBackPressed(this, super::onBackPressed);
         } else {
-            getSupportFragmentManager().popBackStack();
-            binding.navView.getMenu().getItem(0).setChecked(true);
+            fragmentNavigator.back();
+            binding.navView.getMenu().findItem(R.id.nav_dashboard).setChecked(true);
             getSupportActionBar().setTitle(R.string.dashboard);
-            lastSelectedNavItemId = R.id.nav_dashboard;
-            isDashboardActive = true;
         }
     }
 
@@ -107,17 +92,8 @@ public class MainActivity extends BaseInjectActivity<MainPresenter> implements N
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
 
-                int id = item.getItemId();
-
-                if (id == R.id.nav_logout)
-                    showLogoutDialog();
-                else if (id == R.id.nav_about_event) {
-                    Intent intent = new Intent(MainActivity.this, AboutEventActivity.class);
-                    intent.putExtra(AboutEventActivity.EVENT_ID, eventId);
-                    startActivity(intent);
-                } else
-                    loadFragment(id);
-
+                binding.navView.setCheckedItem(item.getItemId());
+                drawerNavigator.selectItem(item);
                 binding.drawerLayout.removeDrawerListener(this);
             }
         });
@@ -133,6 +109,7 @@ public class MainActivity extends BaseInjectActivity<MainPresenter> implements N
     @Override
     public void setEventId(long eventId) {
         this.eventId = eventId;
+        fragmentNavigator.setEventId(eventId);
         binding.navView.getMenu().setGroupVisible(R.id.subMenu, true);
     }
 
@@ -173,60 +150,8 @@ public class MainActivity extends BaseInjectActivity<MainPresenter> implements N
     }
 
     private void loadFragment(int navItemId) {
-        if (lastSelectedNavItemId == navItemId)
-            return;
-
         binding.navView.setCheckedItem(navItemId);
-        lastSelectedNavItemId = navItemId;
 
-        Fragment fragment;
-        FragmentTransaction ft = fragmentManager.beginTransaction();
-
-        switch (navItemId) {
-            case R.id.nav_dashboard:
-                fragment = EventDashboardFragment.newInstance(eventId);
-                break;
-            case R.id.nav_attendees:
-                fragment = AttendeesFragment.newInstance(eventId);
-                break;
-            case R.id.nav_tickets:
-                fragment = TicketsFragment.newInstance(eventId);
-                break;
-            case R.id.nav_events:
-                fragment = EventListFragment.newInstance();
-                break;
-            case R.id.nav_settings:
-                fragment = SettingsFragment.newInstance();
-                break;
-            case R.id.nav_faq:
-                fragment = FaqListFragment.newInstance(eventId);
-                break;
-            default:
-                fragment = EventDashboardFragment.newInstance(eventId);
-                break;
-        }
-
-        getSupportFragmentManager().popBackStack();
-
-        isDashboardActive = navItemId == R.id.nav_dashboard;
-        if (isDashboardActive) {
-            ft.replace(R.id.fragment_container, fragment);
-        } else {
-            ft.add(R.id.fragment_container, fragment).addToBackStack(null);
-        }
-        ft.commit();
-
-    }
-
-    private void showLogoutDialog() {
-        if (logoutDialog == null)
-            logoutDialog = new AlertDialog.Builder(this)
-                .setTitle(R.string.logout_confirmation)
-                .setMessage(R.string.logout_confirmation_message)
-                .setPositiveButton(R.string.ok, (dialog, which) -> getPresenter().logout())
-                .setNegativeButton(R.string.cancel, (dialog, which) -> dialog.dismiss())
-                .create();
-
-        logoutDialog.show();
+        fragmentNavigator.loadFragment(navItemId);
     }
 }
