8fc22b47e7ea69450af4b4547b7a9ca76a94a500
Akshay Kumar <akshaykumarsharma1998aks@gmail.com>
2018-04-14 17:56:09 +0530

    fix: Truncate feedback comment to two lines (#898)

--- app/src/main/java/org/fossasia/openevent/app/core/feedback/list/viewholder/FeedbackViewHolder.java
+++ app/src/main/java/org/fossasia/openevent/app/core/feedback/list/viewholder/FeedbackViewHolder.java
@@ -1,11 +1,10 @@
 package org.fossasia.openevent.app.core.feedback.list.viewholder;
 
-
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 
 import org.fossasia.openevent.app.data.feedback.Feedback;
 import org.fossasia.openevent.app.databinding.FeedbacklistLayoutBinding;
-
 
 public class FeedbackViewHolder extends RecyclerView.ViewHolder {
 
@@ -14,6 +13,15 @@ public class FeedbackViewHolder extends RecyclerView.ViewHolder {
     public FeedbackViewHolder(FeedbacklistLayoutBinding binding) {
         super(binding.getRoot());
         this.binding = binding;
+        binding.commentTextview.setOnClickListener(v -> {
+            if (binding.commentTextview.getEllipsize() == null) {
+                binding.commentTextview.setMaxLines(2);
+                binding.commentTextview.setEllipsize(TextUtils.TruncateAt.END);
+            } else {
+                binding.commentTextview.setEllipsize(null);
+                binding.commentTextview.setMaxLines(Integer.MAX_VALUE);
+            }
+        });
     }
 
     public void bind(Feedback feedback) {
