3fa98f9f8d381fdc4f7cbdbd39b0174bad55724a
sridharjajoo <sridharjajoo@gmail.com>
2018-06-28 21:42:21 +0530

    feat:Add print support for orders

--- app/src/main/java/org/fossasia/openevent/app/core/orders/detail/OrderDetailFragment.java
+++ app/src/main/java/org/fossasia/openevent/app/core/orders/detail/OrderDetailFragment.java
@@ -2,8 +2,11 @@ package org.fossasia.openevent.app.core.orders.detail;
 
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.os.Build;
 import android.os.Bundle;
+import android.print.PrintManager;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
@@ -24,6 +27,7 @@ public class OrderDetailFragment extends BaseFragment implements OrderDetailView
     private static final String ORDER_KEY = "order_id";
     private String orderIdentifier;
     private long eventId;
+    private Order order;
 
     @Inject
     ViewModelProvider.Factory viewModelFactory;
@@ -72,6 +76,20 @@ public class OrderDetailFragment extends BaseFragment implements OrderDetailView
         orderDetailViewModel.getOrder(orderIdentifier, eventId, false).observe(this, this::showOrderDetails);
         orderDetailViewModel.getProgress().observe(this, this::showProgress);
         orderDetailViewModel.getError().observe(this, this::showError);
+        binding.printAction.setOnClickListener(view -> {
+            doPrint();
+        });
+    }
+
+    private void doPrint() {
+        PrintManager printManager;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            printManager = (PrintManager) getActivity().getSystemService(Context.PRINT_SERVICE);
+            String jobName = this.getString(R.string.app_name) + " Document";
+            printManager.print(jobName, new OrderDetailsPrintAdapter(getActivity(), order), null);
+        } else {
+            ViewUtils.showSnackbar(binding.getRoot(), "No Printing Support!");
+        }
     }
 
     @Override
@@ -111,5 +129,6 @@ public class OrderDetailFragment extends BaseFragment implements OrderDetailView
 
     public void showOrderDetails(Order order) {
         binding.setOrder(order);
+        this.order = order;
     }
 }
