b8b4dbed9e5e614abcb08672fa82d811992206ea
Priyanshu Khandelwal <b16027@students.iitmandi.ac.in>
2018-06-30 18:21:25 +0530

    fix: NPE in EncryptionImpl (#1140)
    * fix: NPE in EncryptionImpl
    
    * fix: Minor fixes

--- app/src/main/java/org/fossasia/openevent/app/data/encryption/EncryptionServiceImpl.java
+++ app/src/main/java/org/fossasia/openevent/app/data/encryption/EncryptionServiceImpl.java
@@ -57,6 +57,10 @@ public class EncryptionServiceImpl implements EncryptionService {
 
     @Override
     public String encrypt(String credential) {
+        if (credential == null) {
+            return null;
+        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             return encryptString(credential);
         } else {
@@ -69,6 +73,10 @@ public class EncryptionServiceImpl implements EncryptionService {
 
     @Override
     public String decrypt(String encryptedCredentials) {
+        if (encryptedCredentials == null) {
+            return null;
+        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             return decryptString(encryptedCredentials);
         } else {
@@ -78,24 +86,22 @@ public class EncryptionServiceImpl implements EncryptionService {
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     private String encryptString(String toEncrypt) {
-        if (toEncrypt != null) {
-            try {
-                final KeyStore.PrivateKeyEntry privateKeyEntry = getPrivateKey();
-                if (privateKeyEntry != null) {
-                    final PublicKey publicKey = privateKeyEntry.getCertificate().getPublicKey();
-                    Cipher input = Cipher.getInstance(CYPHER);
-                    input.init(Cipher.ENCRYPT_MODE, publicKey);
-                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                    CipherOutputStream cipherOutputStream = new CipherOutputStream(outputStream, input);
-                    cipherOutputStream.write(toEncrypt.getBytes(ENCODING));
-                    cipherOutputStream.close();
-                    byte[] vals = outputStream.toByteArray();
-                    return Base64.encodeToString(vals, Base64.DEFAULT);
-                }
-            } catch (Exception e) {
-                Timber.e(Log.getStackTraceString(e));
-                return null;
+        try {
+            final KeyStore.PrivateKeyEntry privateKeyEntry = getPrivateKey();
+            if (privateKeyEntry != null) {
+                final PublicKey publicKey = privateKeyEntry.getCertificate().getPublicKey();
+                Cipher input = Cipher.getInstance(CYPHER);
+                input.init(Cipher.ENCRYPT_MODE, publicKey);
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                CipherOutputStream cipherOutputStream = new CipherOutputStream(outputStream, input);
+                cipherOutputStream.write(toEncrypt.getBytes(ENCODING));
+                cipherOutputStream.close();
+                byte[] vals = outputStream.toByteArray();
+                return Base64.encodeToString(vals, Base64.DEFAULT);
             }
+        } catch (Exception e) {
+            Timber.e(Log.getStackTraceString(e));
+            return null;
         }
         return null;
     }
@@ -225,35 +231,26 @@ public class EncryptionServiceImpl implements EncryptionService {
     *
     */
     private byte[] encryptString(String message, SecretKey secret) {
-        Cipher cipher;
-        byte[] cipherText = new byte[0];
-
-        if (message != null) {
-            try {
-                cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-                cipher.init(Cipher.ENCRYPT_MODE, secret);
-                cipherText = cipher.doFinal(message.getBytes("UTF-8"));
-            } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException | UnsupportedEncodingException e) {
-                Timber.w(e);
-            }
-            return cipherText;
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secret);
+            return cipher.doFinal(message.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException | UnsupportedEncodingException e) {
+            Timber.w(e);
+            return null;
         }
-        return null;
     }
 
     private String decryptString(byte[] cipherText, SecretKey secret) {
         /* Decrypt the message, given derived encContentValues and initialization vector. */
-        Cipher cipher = null;
-        String decryptString = null;
-
         try {
-            cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.DECRYPT_MODE, secret);
-            decryptString = new String(cipher.doFinal(cipherText), "UTF-8");
+            return new String(cipher.doFinal(cipherText), "UTF-8");
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException | UnsupportedEncodingException e) {
             Timber.w(e);
+            return null;
         }
-        return decryptString;
     }
 
     private SecretKey generateKey(String password) {
