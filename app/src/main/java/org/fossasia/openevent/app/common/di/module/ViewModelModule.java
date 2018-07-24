caa64c826b45c102832019a4952b41df0ea2a8cc
Sridhar Jajoo <sridharjajoo@gmail.com>
2018-07-24 16:55:53 +0530

    chore: Migrate SignUpPresenter to ViewModel (#1246)

--- app/src/main/java/org/fossasia/openevent/app/common/di/module/ViewModelModule.java
+++ app/src/main/java/org/fossasia/openevent/app/common/di/module/ViewModelModule.java
@@ -6,6 +6,7 @@ import android.arch.lifecycle.ViewModelProvider;
 import org.fossasia.openevent.app.common.di.OrgaViewModelFactory;
 import org.fossasia.openevent.app.core.auth.reset.ResetPasswordViewModel;
 import org.fossasia.openevent.app.core.auth.login.LoginViewModel;
+import org.fossasia.openevent.app.core.auth.signup.SignUpViewModel;
 import org.fossasia.openevent.app.core.event.list.EventsViewModel;
 import org.fossasia.openevent.app.core.event.create.CreateEventViewModel;
 import org.fossasia.openevent.app.core.faq.create.CreateFaqViewModel;
@@ -91,6 +92,11 @@ public abstract class ViewModelModule {
     @ViewModelKey(CreateSponsorViewModel.class)
     public abstract ViewModel bindCreateSponsorViewModel(CreateSponsorViewModel sponsorViewModel);
 
+    @Binds
+    @IntoMap
+    @ViewModelKey(SignUpViewModel.class)
+    public abstract ViewModel bindSignUpViewModel(SignUpViewModel signUpViewModel);
+    
     @Binds
     @IntoMap
     @ViewModelKey(TicketSettingsViewModel.class)
