06fd60f82e5c92827247b0d43df4c885b2cd411f
Sridhar Jajoo <sridharjajoo@gmail.com>
2018-07-25 21:18:38 +0530

    chore: Remove OSS from fdroid build (#1256)
    * chore: Remove OSS from fdroid build
    
    * build fix

---
+++ app/src/fdroid/java/org/fossasia/openevent/app/core/settings/AcknowledgementDecider.java
@@ -0,0 +1,14 @@
+package org.fossasia.openevent.app.core.settings;
+
+import android.content.Context;
+
+public class AcknowledgementDecider {
+
+    public boolean shouldShowAcknowledgement() {
+        return false;
+    }
+
+    public void openAcknowledgementsSection(Context context) {
+        //do nothing
+    }
+}
