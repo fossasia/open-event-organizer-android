a5cbdca36f3f00b566f50f847fc9a6f63e3d441c
sridharjajoo <sridharjajoo@gmail.com>
2018-08-03 15:38:56 +0530

    fix: Disable swiping in ViewPager

---
+++ app/src/main/java/com/eventyay/organizer/core/event/create/EventsViewPager.java
@@ -0,0 +1,31 @@
+package com.eventyay.organizer.core.event.create;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class EventsViewPager extends ViewPager {
+
+    public EventsViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public EventsViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+}
