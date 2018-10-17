23979ac1a792e3b8f3aa5a39158e4daf02487c79
Kushagra Misra <36705914+kush-mish@users.noreply.github.com>
2018-10-17 18:16:40 +0530

    Add import

--- app/src/main/java/com/eventyay/organizer/core/event/list/EventListFragment.java
+++ app/src/main/java/com/eventyay/organizer/core/event/list/EventListFragment.java
@@ -16,6 +16,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
 
 import com.eventyay.organizer.R;
 import com.eventyay.organizer.common.mvp.view.BaseFragment;
