860640e44413c92a7040e7c1c9e480f35c0cb46e
Shridhar Goel <35566748+ShridharGoel@users.noreply.github.com>
2018-11-07 01:55:16 +0530

    Remove unnecessary import statements

--- app/src/main/java/com/eventyay/organizer/core/attendee/history/CheckInHistoryFragment.java
+++ app/src/main/java/com/eventyay/organizer/core/attendee/history/CheckInHistoryFragment.java
@@ -6,8 +6,6 @@ import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
