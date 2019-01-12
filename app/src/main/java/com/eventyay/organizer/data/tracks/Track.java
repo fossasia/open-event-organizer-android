d159aaf619bac18d36dfeb74dffc71bb35afa7bc
Shridhar Goel <35566748+ShridharGoel@users.noreply.github.com>
2019-01-12 12:55:40 +0530

    Remove unnecessary import

--- app/src/main/java/com/eventyay/organizer/data/tracks/Track.java
+++ app/src/main/java/com/eventyay/organizer/data/tracks/Track.java
@@ -23,7 +23,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
