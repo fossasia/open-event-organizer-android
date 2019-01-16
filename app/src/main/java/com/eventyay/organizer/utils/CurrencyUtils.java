b10458e381765c935bb9387582609eea356e3f29
Himanshu Sahni <sahnihimanshu97@gmail.com>
2019-01-16 11:16:15 +0530

    added checks to fix IllegalArgumentException

--- app/src/main/java/com/eventyay/organizer/utils/CurrencyUtils.java
+++ app/src/main/java/com/eventyay/organizer/utils/CurrencyUtils.java
@@ -74,7 +74,7 @@ public final class CurrencyUtils {
             try {
                 String country = locale.getDisplayCountry();
                 Currency currency = null;
-                if( !country.equals("")&&!locale.toString().matches(".*\\d+.*"))
+                if( !Utils.isEmpty(country)&&!locale.toString().matches(".*\\d+.*"))
                     currency = Currency.getInstance(locale);
                 if (currency != null && country != null) {
                     countryCurrencyMap.put(country, currency.toString());
