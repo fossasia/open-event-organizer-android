6506b8d34f07aba08b4c08f5d1c163520323ebb5
Shridhar <shridhar.goel@gmail.com>
2019-01-31 18:28:35 +0530

    Fix multiple display of error message

--- app/src/main/java/com/eventyay/organizer/core/event/create/CreateEventViewModel.java
+++ app/src/main/java/com/eventyay/organizer/core/event/create/CreateEventViewModel.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 import com.eventyay.organizer.common.Constants;
+import com.eventyay.organizer.common.livedata.SingleEventLiveData;
 import com.eventyay.organizer.common.rx.Logger;
 import com.eventyay.organizer.data.Preferences;
 import com.eventyay.organizer.data.event.Event;
@@ -52,10 +53,10 @@ public class CreateEventViewModel extends ViewModel {
     private final List<String> currencyCodesList;
 
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
-    private final MutableLiveData<String> onSuccess = new MutableLiveData<>();
-    private final MutableLiveData<String> onError = new MutableLiveData<>();
-    private final MutableLiveData<Boolean> progress = new MutableLiveData<>();
-    private final MutableLiveData<Void> close = new MutableLiveData<>();
+    private final SingleEventLiveData<String> onSuccess = new SingleEventLiveData<>();
+    private final SingleEventLiveData<String> onError = new SingleEventLiveData<>();
+    private final SingleEventLiveData<Boolean> progress = new SingleEventLiveData<>();
+    private final SingleEventLiveData<Void> close = new SingleEventLiveData<>();
     private final MutableLiveData<Event> eventMutableLiveData = new MutableLiveData<>();
     private final MutableLiveData<ImageUrl> imageUrlMutableLiveData = new MutableLiveData<>();
     private final MutableLiveData<ImageUrl> logoUrlMutableLiveData = new MutableLiveData<>();
