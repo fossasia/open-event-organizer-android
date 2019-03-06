6630e88ae6a20119b91c703bfaccb942f2d4657c
Shridhar <shridhar.goel@gmail.com>
2019-03-07 00:52:00 +0530

    chore: Migrate UpdateTrackPresenter to ViewModel

--- app/src/main/java/com/eventyay/organizer/common/di/module/ViewModelModule.java
+++ app/src/main/java/com/eventyay/organizer/common/di/module/ViewModelModule.java
@@ -28,6 +28,11 @@ import com.eventyay.organizer.core.speakerscall.create.CreateSpeakersCallViewMod
 import com.eventyay.organizer.core.sponsor.create.CreateSponsorViewModel;
 import com.eventyay.organizer.core.ticket.create.CreateTicketViewModel;
 import com.eventyay.organizer.core.ticket.detail.TicketDetailViewModel;
+<<<<<<< Updated upstream
+=======
+import com.eventyay.organizer.core.track.create.CreateTrackViewModel;
+import com.eventyay.organizer.core.track.update.UpdateTrackViewModel;
+>>>>>>> Stashed changes
 
 import dagger.Binds;
 import dagger.Module;
@@ -110,6 +115,11 @@ public abstract class ViewModelModule {
     @IntoMap
     @ViewModelKey(CreateSponsorViewModel.class)
     public abstract ViewModel bindCreateSponsorViewModel(CreateSponsorViewModel sponsorViewModel);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(UpdateTrackViewModel.class)
+    public abstract ViewModel bindUpdateTrackViewModel(UpdateTrackViewModel updateTrackViewModel);
 
     @Binds
     @IntoMap
