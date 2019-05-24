e1e9ce6ded88cf3fd3c54ca7a7c31c72701e5662
Shridhar <shridhar.goel@gmail.com>
2019-05-25 02:08:23 +0530

    chore: Migrate ChartPresenter to ViewModel

--- app/src/main/java/com/eventyay/organizer/common/di/module/ViewModelModule.java
+++ app/src/main/java/com/eventyay/organizer/common/di/module/ViewModelModule.java
@@ -10,6 +10,7 @@ import com.eventyay.organizer.core.auth.login.LoginViewModel;
 import com.eventyay.organizer.core.auth.reset.ResetPasswordViewModel;
 import com.eventyay.organizer.core.auth.signup.SignUpViewModel;
 import com.eventyay.organizer.core.auth.start.StartViewModel;
+import com.eventyay.organizer.core.event.chart.ChartViewModel;
 import com.eventyay.organizer.core.event.copyright.CreateCopyrightViewModel;
 import com.eventyay.organizer.core.event.copyright.update.UpdateCopyrightViewModel;
 import com.eventyay.organizer.core.event.create.CreateEventViewModel;
@@ -56,6 +57,12 @@ public abstract class ViewModelModule {
     @IntoMap
     @ViewModelKey(ResetPasswordViewModel.class)
     public abstract ViewModel bindResetPasswordViewModel(ResetPasswordViewModel resetPasswordViewModel);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ChartViewModel.class)
+    public abstract ViewModel bindChartViewModel(ChartViewModel chartViewModel);
+
 
     @Binds
     @IntoMap
--- app/src/main/java/com/eventyay/organizer/core/event/chart/ChartView.java
+++ app/src/main/java/com/eventyay/organizer/core/event/chart/ChartView.java
@@ -7,6 +7,6 @@ import com.eventyay.organizer.common.mvp.view.Progressive;
 
 public interface ChartView extends Progressive, Erroneous {
 
-    LineChart getChartView();
+    //LineChart getChartView();
 
 }
