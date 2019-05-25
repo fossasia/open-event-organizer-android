3244516d435da9f3f0c1fcf3f8867b018c39f9bb
Shridhar <shridhar.goel@gmail.com>
2019-05-25 13:13:47 +0530

    chore: Migrate ChartPresenter to ViewModel

--- app/src/main/java/com/eventyay/organizer/core/event/chart/ChartView.java
+++ app/src/main/java/com/eventyay/organizer/core/event/chart/ChartView.java
@@ -1,7 +1,5 @@
 package com.eventyay.organizer.core.event.chart;
 
-import com.github.mikephil.charting.charts.LineChart;
-
 import com.eventyay.organizer.common.mvp.view.Erroneous;
 import com.eventyay.organizer.common.mvp.view.Progressive;
 
