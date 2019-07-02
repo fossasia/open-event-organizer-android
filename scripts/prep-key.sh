185dc4adae549169629132b6aff9bb15adcdbc25
iamareebjamal <jamal.areeb@gmail.com>
2019-07-02 22:35:46 +0530

    chore: Simplify signing and APK generation

--- scripts/prep-key.sh
+++ scripts/prep-key.sh
@@ -2,7 +2,7 @@
 set -e
 
 export DEPLOY_BRANCH=${DEPLOY_BRANCH:-development}
-export PUBLISH_BRANCH=${PUBLISH_BRANCH:-build}
+export PUBLISH_BRANCH=${PUBLISH_BRANCH:-master}
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-event-organizer-android" ] || ! [ "$TRAVIS_BRANCH" == "$DEPLOY_BRANCH" -o "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
     echo "We decrypt key only for pushes to the master branch and not PRs. So, skip."
--- scripts/update-apk.sh
+++ scripts/update-apk.sh
@@ -5,7 +5,7 @@ git config --global user.name "Travis CI"
 git config --global user.email "noreply+travis@fossasia.org"
 
 export DEPLOY_BRANCH=${DEPLOY_BRANCH:-development}
-export PUBLISH_BRANCH=${PUBLISH_BRANCH:-build}
+export PUBLISH_BRANCH=${PUBLISH_BRANCH:-master}
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-event-organizer-android" ] || ! [ "$TRAVIS_BRANCH" == "$DEPLOY_BRANCH" -o "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
     echo "We upload apk only for changes in development or master, and not PRs. So, let's skip this shall we ? :)"
@@ -51,16 +51,13 @@ git branch -D apk
 git branch -m apk
 
 # Force push to origin since histories are unrelated
-# git push origin apk --force --quiet > /dev/null
+git push origin apk --force --quiet > /dev/null
 
 # Publish App to Play Store
 if [ "$TRAVIS_BRANCH" != "$PUBLISH_BRANCH" ]; then
     echo "We publish apk only for changes in master branch. So, let's skip this shall we ? :)"
     exit 0
 fi
-
-ls -alh
-exit 0
 
 gem install fastlane
 fastlane supply --aab eventyay-organizer-master-app.aab --track alpha --json_key ../scripts/fastlane.json --package_name $PACKAGE_NAME
