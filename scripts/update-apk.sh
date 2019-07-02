d840dbfe22fa4c22bd90c310567461abd836cb5d
iamareebjamal <jamal.areeb@gmail.com>
2019-07-02 08:07:14 +0530

    chore: Simplify signing and APK generation

--- scripts/update-apk.sh
+++ scripts/update-apk.sh
@@ -5,12 +5,14 @@ git config --global user.name "Travis CI"
 git config --global user.email "noreply+travis@fossasia.org"
 
 export DEPLOY_BRANCH=${DEPLOY_BRANCH:-development}
-export PUBLISH_BRANCH=${PUBLISH_BRANCH:-master}
+export PUBLISH_BRANCH=${PUBLISH_BRANCH:-build}
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-event-orga-app" ] || ! [ "$TRAVIS_BRANCH" == "$DEPLOY_BRANCH" -o "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-event-organizer-android" ] || ! [ "$TRAVIS_BRANCH" == "$DEPLOY_BRANCH" -o "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
     echo "We upload apk only for changes in development or master, and not PRs. So, let's skip this shall we ? :)"
     exit 0
 fi
+
+./gradlew bundlePlayStoreRelease
 
 git clone --quiet --branch=apk https://niranjan94:$GITHUB_API_KEY@github.com/fossasia/open-event-orga-app apk > /dev/null
 cd apk
@@ -21,21 +23,8 @@ else
 	/bin/rm -f eventyay-organizer-dev-*.apk
 fi
 
-\cp -r ../app/build/outputs/apk/playStore/*/**.apk .
-\cp -r ../app/build/outputs/apk/fdroid/*/**.apk .
-\cp -r ../app/build/outputs/apk/playStore/debug/output.json playStore-debug-output.json
-\cp -r ../app/build/outputs/apk/playStore/release/output.json playStore-release-output.json
-\cp -r ../app/build/outputs/apk/fdroid/debug/output.json fdroid-debug-output.json
-\cp -r ../app/build/outputs/apk/fdroid/release/output.json fdroid-release-output.json
-
-# Signing Apps
-
-if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
-    echo "Push to master branch detected, signing the app..."
-    cp app-playStore-release-unsigned.apk app-playStore-release-unaligned.apk
-	jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../scripts/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-playStore-release-unaligned.apk $ALIAS
-	${ANDROID_HOME}/build-tools/27.0.3/zipalign -v -p 4 app-playStore-release-unaligned.apk app-playStore-release.apk
-fi
+\cp -r ../app/build/outputs/*/**.apk .
+\cp -r ../app/build/outputs/*/**.aab .
 
 if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
     for file in app*; do
@@ -62,13 +51,16 @@ git branch -D apk
 git branch -m apk
 
 # Force push to origin since histories are unrelated
-git push origin apk --force --quiet > /dev/null
+# git push origin apk --force --quiet > /dev/null
 
 # Publish App to Play Store
 if [ "$TRAVIS_BRANCH" != "$PUBLISH_BRANCH" ]; then
     echo "We publish apk only for changes in master branch. So, let's skip this shall we ? :)"
     exit 0
 fi
+
+ls -alh
+exit 0
 
 gem install fastlane
 fastlane supply --apk eventyay-organizer-master-app-playStore-release.apk --track alpha --json_key ../scripts/fastlane.json --package_name $PACKAGE_NAME
