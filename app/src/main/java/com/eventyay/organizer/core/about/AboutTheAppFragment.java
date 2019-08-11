845cb13ea1e061a1ed735c95ad6cea6724e80835
Shridhar <shridhar.goel@gmail.com>
2019-08-11 14:56:11 +0530

    feat: Add 'About The App' section

--- app/src/main/java/com/eventyay/organizer/core/about/AboutTheAppFragment.java
+++ app/src/main/java/com/eventyay/organizer/core/about/AboutTheAppFragment.java
@@ -1,6 +1,7 @@
 package com.eventyay.organizer.core.about;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
@@ -14,6 +15,7 @@ import com.eventyay.organizer.R;
 import com.eventyay.organizer.common.mvp.view.BaseFragment;
 import com.eventyay.organizer.databinding.AboutTheAppFragmentBinding;
 import com.eventyay.organizer.utils.BrowserUtils;
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 
 public class AboutTheAppFragment extends BaseFragment {
 
@@ -28,6 +30,12 @@ public class AboutTheAppFragment extends BaseFragment {
         final Context contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.AppTheme);
         LayoutInflater localInflater = inflater.cloneInContext(contextThemeWrapper);
         binding =  DataBindingUtil.inflate(localInflater, R.layout.about_the_app_fragment, container, false);
+
+        binding.ossLicenses.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), OssLicensesMenuActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        });
 
         binding.btnRepo.setOnClickListener(view -> {
             BrowserUtils.launchUrl(getContext(), getString(R.string.github_repo_link));
