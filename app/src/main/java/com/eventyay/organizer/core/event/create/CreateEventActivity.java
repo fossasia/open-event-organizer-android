1a7af5803a68092dfd86a96b46403879aad3fba5
dheerajkotwani <dheeraj.kotwani41@gmail.com>
2020-01-29 19:44:58 +0530

    fix: button disappear on Orientation change

--- app/src/main/java/com/eventyay/organizer/core/event/create/CreateEventActivity.java
+++ app/src/main/java/com/eventyay/organizer/core/event/create/CreateEventActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
